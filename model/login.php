645757942d9d903d5aba55d33c80361e6a2e2529
Fabio Reis <freis1801@gmail.com>
2022-09-05 21:33:45 -0300

    tela de login e cadastro

--- model/login.php
+++ model/login.php
@@ -1,5 +1,5 @@
 <?php
-require("config.php");
+require("../controller/config.php");
 
 
 if (isset($_POST['email']) && isset($_POST['senha']) && $conexao != null) {
@@ -13,10 +13,10 @@ if (isset($_POST['email']) && isset($_POST['senha']) && $conexao != null) {
         $_SESSION['usuario'] = array($user['nome'], $user['adm']);
 
         echo "<script language='javascript' type='text/javascript'>
-            alert('Redirecionando!');window.location ='teste.php'</script>";
+            alert('Redirecionando!');window.location ='../index.html'</script>";
     } else {
         echo "<script language='javascript' type='text/javascript'>
-            alert('Senha ou Email Incorretos!');window.location ='index.html'</script>";
+            alert('Senha ou Email Incorretos!');window.location ='../view/login_page.html'</script>";
     }
 } else {
     echo "<script language='javascript' type='text/javascript'>
