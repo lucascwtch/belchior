6329b1484a52503837f8c627f2e31c70614f1bd4
Gabriel Mendes F. Diacenco <77083828+GMendesss@users.noreply.github.com>
2023-10-14 19:52:49 -0300

    cadastro de produto
    Nesse commit, eu inclui o inserir_produto.php e cadastro_produto.html

---
+++ model/inserir_produto.php
@@ -0,0 +1,22 @@
+<?php
+
+require("../controller/config.php");
+
+
+$tipo_produto = $_POST['tipo'];
+$nome_produto = $_POST['nome'];
+$preco_produto = $_POST['preco'];
+$especificacoes_produto = $_POST['especificacoes'];
+
+$imagem = $_FILES['imagem']['name'];
+$imagem_temp = $_FILES['imagem']['tmp_name'];
+$imagem_destino = "dbimg/" . basename($imagem);
+
+if (move_uploaded_file($imagem_temp, $imagem_destino)) {
+
+    $sql = "INSERT INTO produto (tipo_produto, nome_produto, imagem_produto, preco_produto, especificacoes_produto) VALUES ('$tipo_produto', '$nome_produto', '$imagem', '$preco_produto', '$especificacoes_produto')";
+}
+
+echo "<script language='javascript' type='text/javascript'>
+                alert('Produto cadastrado!');window.location ='../index.html'</script>";
+?>
